5c7fb5662b4afad11538b714de462bbfe6c3d707
Kenzie <hello.kenzie1133@gmail.com>
2025-07-15 02:29:29 +0000

    added groq-key and llma-reply

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import joblib
+from groq import Groq
+
+import os
+os.environ['GROQ_API_KEY'] = os.getenv("groq")
 
 app = Flask(__name__)
 
@@ -13,9 +17,29 @@ def main():
     # db
     return(render_template("main.html"))
 
+@app.route("/llma",methods=["GET","POST"])
+def llma():
+    return(render_template("llma.html"))
+
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
     return(render_template("dbs.html"))
+
+@app.route("/llma_reply",methods=["GET","POST"])
+def llma_reply():
+    q = request.form.get("q")
+    # load model
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llma_reply.html",r=completion.choices[0].message.content))
 
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
