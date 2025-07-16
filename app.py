da42c725eb4f12bd93f05159845a23f61307d038
Kenzie <hello.kenzie1133@gmail.com>
2025-07-16 03:38:22 +0000

    adding telegram

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 from groq import Groq
+import requests
 
 # import os
 # os.environ['GROQ_API_KEY'] = os.getenv("groq")
@@ -28,6 +29,27 @@ def deepseek():
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
     return(render_template("dbs.html"))
+
+@app.route("/telegram",methods=["GET","POST"])
+def telegram():
+
+    domain_url = 'https://gen-ai-dbs.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    # Set the webhook URL for the Telegram bot
+    set_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={domain_url}/webhook"
+    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is running. Please check with the telegram bot. @gemini_tt_bot"
+    else:
+        status = "Failed to start the telegram bot. Please check the logs."
+    
+    return(render_template("telegram.html", status=status))
 
 @app.route("/llma_reply",methods=["GET","POST"])
 def llma_reply():
